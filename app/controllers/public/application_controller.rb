b40d8408cb4ec8f64cd25495799f58efd3ef2896
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2022-12-17 07:24:52 +0000

    firstcommit

---
+++ app/controllers/public/application_controller.rb
@@ -0,0 +1,13 @@
+class Public::ApplicationController < ApplicationController
+  
+
+  # サインイン後の遷移先ページ
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+  
+  # ログイン後の遷移先ページ
+  def after_sign_up_path_for(resource)
+    customers_mypage_path
+  end
+end

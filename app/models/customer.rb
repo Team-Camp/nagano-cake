18b7d9ee8cc27752889074f04841f7e93aecfcb1
EC2 Default User <ec2-user@ip-172-31-22-26.ap-northeast-1.compute.internal>
2022-12-17 09:17:18 +0000

    Add public/registrationsの機能＆バリデーション&灰色のうっすら文字を実装

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,8 +10,8 @@ class Customer < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
-  validates :postal_code, presence: true
+  validates :postal_code, presence: true, numericality: {only_integer: true}
   validates :address, presence: true
-  validates :telephone_number, presence: true
+  validates :telephone_number, presence: true, numericality: {only_integer: true}
 
 end

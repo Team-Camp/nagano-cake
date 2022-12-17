51b667a591601a6e838ce68c273fbbb00ab02cfe
Seiryu1118 <116057980+Seiryu1118@users.noreply.github.com>
2022-12-17 06:54:04 +0000

    [second]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_15_100750) do
+ActiveRecord::Schema.define(version: 2022_12_17_021303) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

b46d5931b2f28ba5018e7b8ab0a1e558d1b9939c
rhama32 <r.hamaoka32915@gmail.com>
2022-12-18 08:30:29 +0000

    hamaoka2

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,9 +1,10 @@
 class Admin::CustomersController < ApplicationController
 
-#管理者(admin_user)のみ、編集と更新が可能　　　
+#管理者(admin_user)のみ、編集と更新が可能
 before_action :admin_user, only: [:edit, :update]
 
 def index
+ @admin_customers = Customer.all.page(params[:page])
 end
 
 def show

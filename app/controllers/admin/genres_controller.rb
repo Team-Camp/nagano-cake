6a4270bfbc7469ed65dd967ebc7f606b295cb193
Seiryu1118 <116057980+Seiryu1118@users.noreply.github.com>
2022-12-18 07:30:29 +0000

    [Finish]

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -8,7 +8,7 @@ class Admin::GenresController < ApplicationController
   def create
     @genre = Genre.new(genre_params)
     @genre.save
-     redirect_to genres_path
+     redirect_to admin_genres_path
   end
 
   def edit
@@ -19,12 +19,12 @@ class Admin::GenresController < ApplicationController
   def update
     @genre = Genre.find(params[:id])
      if @genre.update(genre_params)
-      redirect_to genres_path
+      redirect_to admin_genres_path
      else
       render :edit
      end
   end
-
+ 
   private
 
   def genre_params

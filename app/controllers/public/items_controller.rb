971423634b10c172d98e132129a60c5263f81f53
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2022-12-21 14:49:41 +0000

    フッター追加

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -6,6 +6,7 @@ class Public::ItemsController < ApplicationController
   def index
     @genres = Genre.all
     @items = Item.all 
+    @item = Item.page(params[:page])
     if params[:genre_id]
       @genre = Genre.find(params[:genre_id])
       @items = @genre.items

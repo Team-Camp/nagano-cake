10a4adcae6ee4a1a9343d0fa517528a99c0cc27e
rhama32 <r.hamaoka32915@gmail.com>
2022-12-21 03:29:09 +0000

    概算コミット③

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,18 +3,21 @@ class Public::CartItemsController < ApplicationController
 
 #ショッピングカート内の商品表示
  def index
-  @cart_items = current_cart.cart_items.includes([:product])
-  @prices = Price.find
+  @cart_items = current_cart.cart_items.includes([:item])
+  @prices = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
   @amounts = current_cart.cart_items.includes([:amount])
  end
 
- def show
- end
-
- def edit
- end
-
  def update
+ end
+
+ def destroy
+ end
+
+ def destroy_all
+ end
+
+ def create
  end
 
 end

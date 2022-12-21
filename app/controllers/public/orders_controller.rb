d432bf9681de1be3e1347a5cb992ce3552f503ba
Seiryu1118 <116057980+Seiryu1118@users.noreply.github.com>
2022-12-21 07:38:43 +0000

    途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,6 +7,26 @@ class Public::OrdersController < ApplicationController
  
  def confirm
   @order = Order.new(order_params) 
+  if params[:order][:address_option] == "0"
+   @order.post_code = current_member.post_code
+   @order.address = current_member.address
+   @order.name = current_member.last_name + current_member.first_name 
+  elsif params[:order][:address_option] == "1"
+   ship = Address.find(params[:order][:customer_id])
+   @order.post_code = ship.post_code
+   @order.address = ship.address
+   @order.name = ship.name 
+  elsif params[:order][:address_option] = "2"
+   @order.post_code = params[:order][:shipping_post_code]
+   @order.address = params[:order][:shipping_address]
+   @order.name = params[:order][:shipping_name]
+  else
+   render 'new'
+  end
+  
+  @cart_items = current_customer.cart_items.all
+  @order.customer_id = current_customer.id
+  
  end
  
  def complete
@@ -14,10 +34,15 @@ class Public::OrdersController < ApplicationController
  
  def create
   @orders = Order.new(order_params)
+  @order.customer_id = current_customer.id
+  @order.save
+  
+  
   
  end
  
  def index
+   
  end
  
  def show
